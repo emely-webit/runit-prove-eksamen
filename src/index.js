ab245fd052e8220feef9fdae55e069f68043b019
Emely <emelybuchholt@gmail.com>
2020-08-24 13:02:17 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
