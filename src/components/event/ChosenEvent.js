564cfd3cca746a877b0d7955967a89976952c833
Emely <emelybuchholt@gmail.com>
2020-08-25 14:17:28 +0200

    single event faerdig

--- src/components/event/ChosenEvent.js
+++ src/components/event/ChosenEvent.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const ChosenEvent = () => {
-    return (
-        <div>
-            udvalgt event
-        </div>
-    )
-}
+  return (
+    <section className="ml-3 mt-4 p-3 bg-white w-85 row">
+      <img
+        className="img-fluid col-5"
+        src="/img/foresttothebay.jpg"
+        alt="relmkg"
+      />
+      <article className="col-7">
+        <p className="bg-primary p-2 text-uppercase text-white font-weight-bold m-0">
+          antal pladser tilbage
+        </p>
+        <h1 className="font-weight-bold my-0 h2">Titel på event</h1>
+        <small className="font-italic">Dato</small>
+        <p className="mt-3">
+          beskrivelse: Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Laborum, officiis! Consequuntur, eligendi aspernatur necessitatibus
+          minima architecto ullam voluptas, quod corporis dignissimos possimus
+          sapiente incidunt officiis. Iste, consequatur! Incidunt, optio
+          tempora!
+        </p>
+        <p className="font-weight-bold my-0">Region:</p>
+        <p className="font-weight-bold my-0">Distance:</p>
+        <p className="font-weight-bold h4 my-0">Pris</p>
+        <form>
+          <input className="form-control w-50 rounded-0 d-inline" type="email" name="tilmeldEvent" id="tilmeldEvent" placeholder="tilmeldelse email" />
+          <input className="py-2 bg-primary border-0 rounded-0 text-white px-5" type="submit" value="Tilmeld"/>
+        </form>
+      </article>
+    </section>
+  );
+};
 
-export default ChosenEvent
+export default ChosenEvent;
