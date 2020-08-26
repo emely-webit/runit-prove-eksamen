fd3e5acd00e1c4eea23e748788df2318bd60596e
Emely <emelybuchholt@gmail.com>
2020-08-26 10:59:28 +0200

    random sponsor på forsiden vises nu

--- src/components/layout/NextRun.js
+++ src/components/layout/NextRun.js
@@ -1,22 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// import moment from "moment";
 import { Link } from "react-router-dom";
 
 const NextRun = () => {
+  const [nextRun, setNextRun] = useState();
+
+  useEffect(() => {
+    let url = "http://localhost:5021/event";
+    fetch(url, {
+      method: "GET",
+    })
+    .then((data) => {
+      return data.json();
+    })
+    .then((jsonData) => {
+      console.log(jsonData)
+      const sorted = jsonData.sort((a,b) => { return a.date - b.date})
+      console.log(sorted)
+      setNextRun(sorted[0]);
+    })
+    .catch((err) => {
+      alert('Der gik noget galt ' + err);
+    })
+  },[]);
+
+  // const sorted = [...nextRun].sort((a,b) => {return a.date - b.date})
+  // console.log(sorted)
+
+  let nextRunSection = "";
+
+
+    if (nextRun !== undefined) {
+      nextRunSection = (
+        <>
+          <div className="bg-dark p-2">
+            <p className="text-uppercase text-white font-weight-bold">
+              næste løb - 55/70 pladser optaget
+            </p>
+          </div>
+          <div className="bg-white p-2">
+            <p className="bg-primary p-1 text-uppercase text-white font-weight-bold">
+              {nextRun.antalpladser}
+            </p>
+            <h2 className="font-weight-bold text-uppercase">{nextRun.titel}</h2>
+            <small className="font-italic">{nextRun.dato}</small>
+            <p>{nextRun.beskrivelse}</p>
+            <p className="font-weight-bold">Pris{nextRun.pris}kr</p>
+            <Link className="btn" to={`/event/${nextRun._id}`}>
+              Læs mere
+            </Link>
+          </div>
+        </>
+      );
+    }
+  
+
+
   return (
     <article>
-      <div className="bg-dark p-2">
-        <p className="text-uppercase text-white font-weight-bold">
-          næste løb - 55/70 pladser optaget
-        </p>
-      </div>
-      <div className="bg-white p-2">
-          <p className="bg-primary p-1 text-uppercase text-white font-weight-bold">antal pladser tilbage</p>
-          <h2 className="font-weight-bold text-uppercase">Titel</h2>
-          <small className="font-italic">dato</small>
-          <p>Beskrivelse: Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quos consequuntur exercitationem architecto officiis dolorum itaque ipsum expedita ipsa hic!</p>
-          <p className="font-weight-bold">Pris</p>
-          <Link className="btn" to="/event">Læs mere</Link>
-      </div>
+      {nextRunSection}
     </article>
   );
 };
--- src/components/layout/RandomSpons.js
+++ src/components/layout/RandomSpons.js
@@ -1,15 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const RandomSpons = () => {
+  const [random, setRandom] = useState();
+  useEffect(() => {
+    let url = "http://localhost:5021/sponsor";
+    fetch(url, {
+      method: "GET",
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((jsonData) => {
+        let randomNumber = Math.floor(Math.random(0)*jsonData.length);
+        setRandom(jsonData[randomNumber]);
+      })
+      .catch((err) => {
+        alert("der er sket en fejl da du hentede sponsor " + err);
+      });
+  }, []);
+
+  let randomSponsSection = "";
+
+  if (random !== undefined) {
+    if (random.sponsorkategori.kategori === "Guld") {
+      randomSponsSection = (
+        <article className="bg-info text-center">
+          <p className="h3 font-weight-bold">Sponsorer</p>
+          <img
+            className="img-fluid w-50"
+            src={`http://localhost:5021/images/sponsorer/${random.logo}`}
+            alt="her skal titel være"
+          />
+        </article>
+      );
+    } else if (random.sponsorkategori.kategori === "Sølv") {
+      randomSponsSection = (
+        <article className="bg-secondary text-center">
+          <p className="h3 font-weight-bold">Sponsorer</p>
+          <img
+            className="img-fluid w-50"
+            src={`http://localhost:5021/images/sponsorer/${random.logo}`}
+            alt="her skal titel være"
+          />
+        </article>
+      );
+    } else {
+      randomSponsSection = (
+        <article className="bg-white text-center">
+          <p className="h3 font-weight-bold">Sponsorer</p>
+          <img
+            className="img-fluid w-50"
+            src={`http://localhost:5021/images/sponsorer/${random.logo}`}
+            alt="her skal titel være"
+          />
+        </article>
+      );
+    }
+  }
+
   return (
-    <article className="bg-info text-center">
-      <p className="h3 font-weight-bold">Sponsorer</p>
-      <img
-        className="img-fluid w-50"
-        src="/img/nike.png"
-        alt="her skal titel være"
-      />
-    </article>
+    <>
+      {randomSponsSection}
+    </>
   );
 };
 
