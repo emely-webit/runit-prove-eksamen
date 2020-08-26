f1c57c900b9b8cb06b0fc6d0ff43e6f2a80692bd
Emely <emelybuchholt@gmail.com>
2020-08-26 15:50:49 +0200

    om runit siden er nu lavet

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -1,28 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const AboutUs = () => {
-    return (
-      <section className="ml-lg-3 mt-4 p-3 bg-white w-85 row">
-        <img
-          className="img-fluid col-lg-5 col-12"
-          src="/img/foresttothebay.jpg"
-          alt="relmkg"
-        />
-        <article className="col-lg-7 col-12">
-          <p className="bg-primary p-2 text-uppercase text-white font-weight-bold m-0">
-            Om RUN'IT
-          </p>
-          <h1 className="font-weight-bold my-lg-0 mt-3 h2">Overskrift</h1>
-          <p className="mt-3">
-            beskrivelse: Lorem ipsum dolor sit amet, consectetur adipisicing
-            elit. Laborum, officiis! Consequuntur, eligendi aspernatur
-            necessitatibus minima architecto ullam voluptas, quod corporis
-            dignissimos possimus sapiente incidunt officiis. Iste, consequatur!
-            Incidunt, optio tempora!
-          </p>
-        </article>
-      </section>
-    );
-}
+  const [omOs, setOmOs] = useState({});
 
-export default AboutUs
+  useEffect(() => {
+    let url = "http://localhost:5021/omos";
+
+    fetch(url, {
+      method: "GET",
+    })
+    .then(data => {
+      return data.json();
+    })
+    .then(jsonData => {
+      setOmOs(jsonData[0]);
+    })
+    .catch(err => {
+      alert('Der er sket en fejl ' + err);
+    })
+  }, []);
+
+  return (
+    <section className="ml-lg-3 mt-4 p-3 bg-white w-85 row">
+      <img
+        className="img-fluid col-lg-5 col-12"
+        src={`http://localhost:5021/images/omos/${omOs.billede}`}
+        alt="relmkg"
+      />
+      <article className="col-lg-7 col-12">
+        <p className="bg-primary p-2 text-uppercase text-white font-weight-bold m-0">
+          Om RUN'IT
+        </p>
+        <h1 className="font-weight-bold my-lg-0 mt-3 h2">{omOs.overskrift}</h1>
+        <p className="mt-3">{omOs.beskrivelse}</p>
+      </article>
+    </section>
+  );
+};
+
+export default AboutUs;
